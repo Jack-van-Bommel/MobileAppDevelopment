042bff926878ed1d065788025d4174a83661b480
Jack <9019233@student.zadkine.nl>
2026-05-28 13:24:35 +0200

    backup

--- MobileAppDevApp_1/app/src/main/java/com/example/mobileappdevapp_1/MainActivity.java
+++ MobileAppDevApp_1/app/src/main/java/com/example/mobileappdevapp_1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mobileappdevapp_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GoToSite(View view) {
+        Intent browserIntent = new Intent("android.intent.action.VIEW"
+                                        , Uri.parse("https://www.speedtest.net/nl"));
+        startActivity(browserIntent);
+    }
+
+    public void page_2(View view) {
+        Intent pageIntent = new Intent()
     }
 }
